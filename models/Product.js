9738ce45476f09b4d6f9c6997d0892acf93999f0
r-alo <email@example.com>
2022-08-14 16:46:45 -0500

    set up models / fixed host on connection.js / fixed .env credentials

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      validate: {
+        isDecimal: true
+      },
+      allowNull: false
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      },
+      allowNull: false
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
